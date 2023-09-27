658258dd6d16d0b91b32642622ba1f8e1f748232
linyu114 <11056006@ntub.edu.tw>
2023-09-27 15:51:18 +0800

    改改改

--- mblog0927/settings.py
+++ mblog0927/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0-9o8m4hu2w#&z8s-a3h8q*qk2wfxf_uv4z9$n7mj6-d0@mp_j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 

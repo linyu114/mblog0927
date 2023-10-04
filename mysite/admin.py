9311d7d146df31ab435542c3d04497b77d056f55
linyu114 <11056006@ntub.edu.tw>
2023-10-04 15:51:23 +0800

    新增Post文件跟管理介面

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from mysite.models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','slug','pub_date')#讓管理者可以看到這些
+    
+admin.site.register(Post,PostAdmin) #之後連到 http://localhost:8000/admin
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True) #自動取得現在時間
+    
+    class Meta:
+        ordering = ('-pub_date',) #用pub_date去排序 -反向排序
+    
+    def __str__(self):
+        return self.title #印出標題就好
+    

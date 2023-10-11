90e959e6b50761cfba3cfcfaa42160b2f8d0a940
linyu114 <11056006@ntub.edu.tw>
2023-10-11 16:23:54 +0800

    2023年10月11日16:24

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from mysite.models import Post
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):#名稱自己定義(homepage)
+    posts = Post.objects.all() #select * from post
+    now = datetime.now()
+    return render (request, 'index.html',locals())
+
+def showpost(request,slug):
+    post = Post.objects.get(slug=slug)
+    #select*from post where slug=%slug
+    return render (request, 'post.html',locals())
+    
+
+'''
+def homepage(request):#名稱自己定義(homepage)
+    posts = Post.objects.all() #select * from post
+    post_list = list()
+    for counter, post in enumerate(posts):
+        post_list.append(f'No.{counter}-{post} <br>') #br為網頁換行
+    return HttpResponse(post_list)
+'''

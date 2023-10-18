3f5437b3916f1343ceb1551ca3e1994b20766da5
linyu114 <11056006@ntub.edu.tw>
2023-10-18 16:16:04 +0800

    2023/10/18 04:16PM

--- mysite/views.py
+++ mysite/views.py
@@ -4,21 +4,21 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import redirect
 # Create your views here.
-def homepage(request):#名稱自己定義(homepage)
-    posts = Post.objects.all() #select * from post
+def homepage(request):
+    posts = Post.objects.all()
     now = datetime.now()
-    return render (request, 'index.html',locals())
-
-def showpost(request,slug):
+    return render(request, 'index.html', locals())
+    
+def showpost(request, slug):
     try:
-        post = Post.objects.get(slug=slug)
-        if post !=None:
-            return render (request, 'post.html',locals())
+        post = Post.objects.get(slug=slug) 
+        if post != None:
+            return render(request, 'post.html', locals())
         else:
-            return redirect("/")
+            return redirect("/")    
     except:
         return redirect("/")
-    #select*from post where slug=%slug
+    #select * from post where slug=%slug
     
 
 '''
